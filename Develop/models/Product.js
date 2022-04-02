c243417203ab3122c6f2f7177631bbc641657410
Rich Arce <raarce118@gmail.com>
2022-04-01 20:09:52 -0700

    added to category and products model

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+     id: {
+       type: DataTypes.INTEGER,
+       allowNull: false,
+       primaryKey: true,
+       auto_increment: true
+       },
+
+     produce_name: {
+       type: DataTypes.STRING,
+       allowNull: false
+       },
+
+     category_id: {
+       type: DataTypes.INTEGER,
+           references: {
+               model: 'category',
+               key: 'id',
+               unique: false
+               }
+       }, 
+       
+     stock: {
+       type: DataTypes.INTEGER,
+       allowNull: false,
+       defaultValue: 5,
+       validate: { isNumeric: true, }
+       }, 
+
+     price: {
+       type: DataTypes.DECIMAL,
+       allowNull: true,
+       validate: { isDecimal: true }
+       },  
   },
   {
     sequelize,
