b9ef6f5d16590dfd8dc0e90a43c6bbe793dcf870
Rich Arce <raarce118@gmail.com>
2022-04-02 12:51:23 -0700

    added to product and tag models

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,31 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+   id: {
+     type: DataTypes.INTEGER,
+     allowNull: false,
+     primaryKey: true,
+     autoIncrement: true
+   },
+
+   tag_id: {
+     type: DataTypes.INTEGER,
+     references: {
+       model: 'product',
+       key: 'id',
+       unique: false
+     }
+
+   },
+
+   product_id: {
+     type: DataTypes.INTEGER,
+      references: {
+          model: 'product',
+          key: 'id',
+          unique: false
+       }
+    }
   },
   {
     sequelize,
